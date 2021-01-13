7dffc92b3678b637a0312489c42747925b01f50c
NEGALITOR <tahatas123@gmail.com>
2021-01-13 12:04:45 -0500

    Quick Character Movement
    Basic character movement (may change)

---
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float pSpeed = 10f;
+    public float jumpPower = 7f;
+    public bool isGrounded = false;
+
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+    }
+
+    void Move()
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hAxis, 0f, vAxis);
+        rb.AddForce(movement * pSpeed * Time.deltaTime, ForceMode.VelocityChange);
+    }
+
+    void Jump()
+    {
+        if (isGrounded && Input.GetKeyUp(KeyCode.Space))
+        {
+            rb.AddForce(new Vector3(0, jumpPower, 0) * 50, ForceMode.Acceleration);
+            isGrounded = false;
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
+    }
+}
