aaeded89b79a4911b425622bef7a9dc96ee7fbb9
SentientBroom0 <73441253+SentientBroom0@users.noreply.github.com>
2021-01-19 13:10:56 -0500

    Portal works, kinda
    Spamming back and forth
    Something is wrong with the offset

---
+++ Assets/Player/Scripts/Portal.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public GameObject otherPortal;
+    private bool cooldown = true;
+    private Vector3 offSet;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 offSet = new Vector3(0.0f, 0.0f, 25.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && cooldown == true)
+        {
+            other.transform.position = otherPortal.transform.position + offSet;
+            Invoke("CoolDown", 1.0f);
+        }
+    }
+
+    void CoolDown()
+    {
+        cooldown = false;
+    }
+}
