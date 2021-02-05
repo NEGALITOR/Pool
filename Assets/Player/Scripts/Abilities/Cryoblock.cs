f70ec8ac889d9df3ac2a6c53c538e79222d50c16
NEGALITOR <tahatas123@gmail.com>
2021-02-04 22:19:02 -0500

    Updated Cryoblock

--- Assets/Player/Scripts/Abilities/Cryoblock.cs
+++ Assets/Player/Scripts/Abilities/Cryoblock.cs
@@ -9,6 +9,7 @@ public class Cryoblock : MonoBehaviour
     public GameObject cryoBlock;
     public List<GameObject> spawnedBlock = new List<GameObject>{ null };
     public bool charge = false;
+    private float step = 200f;
 
     // Start is called before the first frame update
     void Start()
@@ -26,21 +27,23 @@ public class Cryoblock : MonoBehaviour
 
         if(charge == true)
         {
-            if (RCJ.currentHitObject != null && ((RCJ.currentHitObject.CompareTag("CryoPool") || RCJ.currentHitObject.CompareTag("Water")) && Input.GetKeyDown(KeyCode.E)))
+            if (RCJ.currentHitObject != null && ((RCJ.currentHitObject.CompareTag("CryoPool") || RCJ.currentHitObject.CompareTag("Water")) && Input.GetKeyUp(KeyCode.E)))
             {
                 foreach (GameObject block in spawnedBlock)
                 {
                     IEnumerator Sub()
                     {
                         spawnedBlock[0].GetComponent<Animator>().SetBool("isActive", true);
-                        yield return new WaitForSeconds(2f); //spawnedBlock[0].GetComponent<Animation>()["Cryoblock Submerge"].length
+                        yield return new WaitForSeconds(1f); //spawnedBlock[0].GetComponent<Animation>()["Cryoblock Submerge"].length
                         Destroy(block);
                     }
 
                     StartCoroutine(Sub());
                 }
-                
-                spawnedBlock.Insert(0, Instantiate(cryoBlock, transform.position - new Vector3(0, 1.5f, 0), transform.rotation));
+
+                gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, gameObject.transform.position + new Vector3(0, 1.1f, 0), Time.deltaTime * step); ;
+                spawnedBlock.Insert(0, Instantiate(cryoBlock, transform.position - new Vector3(0, 2f, 0), transform.rotation));
+                spawnedBlock[0].transform.position = Vector3.MoveTowards(spawnedBlock[0].transform.position, spawnedBlock[0].transform.position + new Vector3(0, 1f, 0), Time.deltaTime * step);
                 spawnedBlock.RemoveAll(spawnedBlock => spawnedBlock == null);
                 spawnedBlock[0].GetComponent<Animator>().SetBool("isActive", false);
 
